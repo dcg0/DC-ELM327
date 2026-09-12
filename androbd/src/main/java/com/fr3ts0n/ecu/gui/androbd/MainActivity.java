e32906dca1c55ef9ebac5341c949d99c97b5e0c1
dcg0 <dddcg0@gmail.com>
2026-09-12 10:53:12 -0600

    DC-ELM327: Enlazar la acción del menú superior con DashboardActivity

--- androbd/src/main/java/com/fr3ts0n/ecu/gui/androbd/MainActivity.java
+++ androbd/src/main/java/com/fr3ts0n/ecu/gui/androbd/MainActivity.java
@@ -948,7 +948,14 @@ public class MainActivity extends PluginManager
      * Compatible with Android 4.1+ using standard menu handling.
      */
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
+    public 
+    @Override
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+        if (item.getItemId() == getResources().getIdentifier("action_dashboard", "id", getPackageName())) {
+            startActivity(new android.content.Intent(this, org.cz.android.androbd.ui.DashboardActivity.class));
+            return true;
+        }
+            MenuItem item)
     {
         int id = item.getItemId();
         {
