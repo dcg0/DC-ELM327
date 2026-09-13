fff93826e2af609e0278439e7249e889eb899db0
dcg0 <dddcg0@gmail.com>
2026-09-13 00:01:06 -0600

    Reintegrar modulo plugin con codigo real de AndrOBD-libplugin

--- androbd/src/main/java/com/fr3ts0n/ecu/gui/androbd/MainActivity.java
+++ androbd/src/main/java/com/fr3ts0n/ecu/gui/androbd/MainActivity.java
@@ -948,16 +948,13 @@ public class MainActivity extends PluginManager
      * Compatible with Android 4.1+ using standard menu handling.
      */
     @Override
-    public 
-    @Override
-    public boolean onOptionsItemSelected(android.view.MenuItem item) {
-        if (item.getItemId() == getResources().getIdentifier("action_dashboard", "id", getPackageName())) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if (id == getResources().getIdentifier("action_dashboard", "id", getPackageName())) {
             startActivity(new android.content.Intent(this, org.cz.android.androbd.ui.DashboardActivity.class));
             return true;
         }
-            MenuItem item)
-    {
-        int id = item.getItemId();
         {
             // Display and UI Management
             if(id == R.id.day_night_mode) {
